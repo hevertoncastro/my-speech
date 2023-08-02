6575366690eee6d1416b3c7bd4118ab5e73a9f2f
Heverton Castro <heverton.silva.ext@dasa.com.br>
2023-08-02 14:50:55 -0300

    Text to speech functionality

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Speech from 'expo-speech';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const speak = () => {
+    const thingToSay = 'Tentando falar português';
+    Speech.speak(thingToSay, {
+      language: 'pt-BR',
+      pitch: 1,
+      rate: 1,
+      voice: 'pt-br-x-afs#female_3-local',
+      volume: 1,
+    });
+    Speech.getAvailableVoicesAsync().then((voices) => {
+      console.log(JSON.stringify(voices, null, 2));
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="Press to hear some words" onPress={speak} />
     </View>
   );
 }
